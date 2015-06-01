68c9ae207dd015e09acb3203f850d652325b28d5
ssatpati <ssatpati@ebay.com>
2015-05-31 18:09:30 -0700

    Random Key for next iteration

--- misc/mumbler_aggregator.py
+++ misc/mumbler_aggregator.py
@@ -40,7 +40,7 @@ def aggregate(word1, hosts, zip_dir=ZIP_DIR):
                                                                  probability))
 
     # Write to Output File
-    with open(os.path.join(ZIP_DIR, OUT_DIR, OUT_FILE), "w") as f:
+    with open(os.path.join(ZIP_DIR, OUT_DIR, OUT_FILE), "a") as f:
         f.write("%s\t%s\t%s\t%s\t%s\n" %(word1, dd_merged.get(word1), random_key, random_key_value, probability))
 
 
